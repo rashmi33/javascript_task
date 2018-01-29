10f73a73db5ed8491cee810203952fc80a1ae676
rashmi33 <rashmi@gkmit.co>
2018-01-29 12:18:47 +0530

    rewrite javascript for popup modal

--- assets/popup.js
+++ assets/popup.js
@@ -1,24 +1,30 @@
-window.onload = function(){
-	var modal = document.getElementById('myModal');
-	var btn1 = document.getElementById("sign-up1");
-	var btn2 = document.getElementById("sign-up2");
-	var btn3 = document.getElementById("sign-up3");
+function login()
+{
+	var form = document.getElementById('myModal');
+	var btn1 = document.getElementById("signup1");
+	var btn2 = document.getElementById("signup2");
+	var btn3 = document.getElementById("signup3");
 	var span = document.getElementsByClassName("close")[0];
 	btn1.onclick = function() {
-	  modal.style.display = "block";
-	}
-	btn2.onclick = function() {
-	  modal.style.display = "block";
-	}
-	btn3.onclick = function() {
-	  modal.style.display = "block";
-	}
+    form.style.display = "block";
+  }
+
+  btn2.onclick = function() {
+    form.style.display = "block";
+  }
+
+  btn3.onclick = function() {
+    form.style.display = "block";
+  }
+
 	span.onclick = function() {
-	  modal.style.display = "none";
-	}
-	window.onclick = function(event) {
-	  if (event.target == modal) {
-	    modal.style.display = "none";
-	  }
-	}
+    form.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    if (event.target == form) {
+        form.style.display = "none";
+    }
+  }
+
 }
